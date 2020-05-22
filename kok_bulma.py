ed6178031b6c503be1800079ccd2acb61b4f4f0b
Mrtcnakcy <muratcan_burak@hotmail.com>
2020-05-22 16:45:22 +0300

    py1

---
+++ kok_bulma.py
@@ -0,0 +1,15 @@
+a = int(input("Denklemin a değeri:"))
+
+b = int(input("Denklemin b değeri:"))
+
+c = int(input("Denklemin c değeri:"))
+
+
+delta  = b ** 2 - 4  * a * c
+
+x1  =  (-b - delta ** 0.5)/ (2 * a)
+
+x2 = (-b + delta ** 0.5) / (2 * a)
+
+
+print("Birinci Kök : {}\nİkinci Kök : {}".format(x1,x2))

3f4b8aadf94e2d2783b81ed6614731aea11ea9ec
Mrtcnakcy <muratcan_burak@hotmail.com>
2020-05-30 18:03:09 +0300

    pyK2

--- kulanıcı_giris.py
+++ kulanıcı_giris.py
@@ -4,15 +4,23 @@ print("***********"
 
 sys_kullanici_adi = "muratcan"
 sys_parola = "159"
+giris_hakki=3
 
-kullanici_adi = input("Kullanıcı adı: ")
-parola = input("Şifre: ")
-
-if(kullanici_adi == sys_kullanici_adi and sys_parola!=parola):
-    print("Parola Hatalı!")
-elif(kullanici_adi != sys_kullanici_adi and sys_parola == parola):
-    print("Kullanıcı Adı Hatalı!")
-elif(kullanici_adi != sys_kullanici_adi and sys_parola != parola):
-    print("Kullanıcı Adı ve Parola Hatalı!")
-else:
-    print("Giriş Başarılı.")
+while True:
+    kullanici_adi = input("Kullanıcı adı: ")
+    parola = input("Şifre: ")
+    if(kullanici_adi != sys_kullanici_adi and sys_parola == parola):
+        print("Kullanıcı Adı Hatalı!")
+        giris_hakki -= 1
+    elif(kullanici_adi == sys_kullanici_adi and sys_parola!=parola):
+        print("Parola Hatalı!")
+        giris_hakki -= 1
+    elif (kullanici_adi != sys_kullanici_adi and sys_parola != parola):
+        print("Kullanıcı Adı ve Parola Hatalı!")
+        giris_hakki -= 1
+    else:
+        print("Giriş Başarılı")
+        break
+    if(giris_hakki==0):
+        print("Giriş Hakkınız bitti")
+        break
